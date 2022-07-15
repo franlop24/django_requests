1c7aafe1d3cf4e451afb18114c3a087abe688288
Francisco Lopez <franlopbri@gmail.com>
2022-07-15 13:02:13 -0500

    implementando form ara crear token

--- djrequests/market/urls.py
+++ djrequests/market/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('products', views.index, name='index'),
+    path('ftoken', views.form_token, name='form_token'),
+    path('token', views.token, name='token'),
 ]
--- djrequests/market/views.py
+++ djrequests/market/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'market/index.html')
+
+def form_token(request):
+    return render(request, 'market/form_token.html')
+
+def token(request):
+    return render(request, 'market/token.html')
