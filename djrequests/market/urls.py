b20275589c67537b3010742d233380f90519d973
Francisco Lopez <franlopbri@gmail.com>
2022-08-05 12:46:50 -0500

    lsta de products

--- djrequests/market/urls.py
+++ djrequests/market/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products', views.index, name='index'),
+    path('products/<str:token>', views.index, name='index'),
     path('ftoken', views.form_token, name='form_token'),
     path('token', views.token, name='token'),
 ]
--- djrequests/market/views.py
+++ djrequests/market/views.py
@@ -5,8 +5,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'market/index.html')
+def index(request, token):
+
+    url = "https://franlopsmarket.herokuapp.com/franlops-market/api/products/all"
+
+    payload={}
+    headers = {
+    'Authorization': f'Bearer {token}'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    products = response.json()
+
+    return render(request, 'market/index.html', {'products': products})
 
 def form_token(request):
     return render(request, 'market/form_token.html')
